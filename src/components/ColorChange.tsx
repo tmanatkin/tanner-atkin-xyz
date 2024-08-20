f54505da27b705bd266ccd1b9168ebd0eed7f558
tanneratkin <tmanatkin@gmail.com>
2024-08-20 15:31:27 -0600

    updated buttons and disabling them when active

--- src/components/ColorChange.tsx
+++ src/components/ColorChange.tsx
@@ -1,33 +1,45 @@
+import { useEffect } from 'react';
+
 const ColorChange = () => {
-  let currentPosition = '0% 0%';
+  // disable first button on load
+  useEffect(() => {
+    (
+      document.getElementById('color-change-button-0') as HTMLButtonElement
+    ).disabled = true;
+  }, []);
 
-  function changeGradient(newPosition: string): void {
+  // initialize variables
+  const positions = ['0% 0%', '33% 33%', '67% 67%', '100% 100%'];
+  let currentPosition = 0;
+
+  function changeGradient(newPosition: number): void {
     // reset position to loop end to end
-
-    if (currentPosition === '67% 67%' && newPosition === '0% 0%') {
-      newPosition = '100% 100%';
-    } else if (currentPosition === '0% 0%' && newPosition === '67% 67%') {
-      currentPosition = '100% 100%';
+    if (currentPosition === 2 && newPosition === 0) {
+      newPosition = 3;
+    } else if (currentPosition === 0 && newPosition === 2) {
+      currentPosition = 3;
     }
 
     // set starting position
     document.documentElement.style.setProperty(
       '--gradient-animation-position-start',
-      `${currentPosition}`,
+      `${positions[currentPosition]}`,
     );
 
     // set ending (new current) position
     document.documentElement.style.setProperty(
       '--gradient-animation-position-end',
-      `${newPosition}`,
+      `${positions[newPosition]}`,
     );
 
-    if (newPosition === '100% 100%') {
-      currentPosition = '0% 0%';
+    // set new current position
+    if (newPosition === 3) {
+      currentPosition = 0;
     } else {
       currentPosition = newPosition;
     }
 
+    // get elements and buttons
     const elements = document.querySelectorAll('.gradient-background');
     const buttons = document.querySelectorAll('.color-change-button');
 
@@ -41,16 +53,24 @@ const ColorChange = () => {
       (button as HTMLButtonElement).disabled = true;
     });
 
-    // wait till animation ends to remove
+    // wait till animation ends
     elements.forEach((element) => {
       element?.addEventListener(
         'animationend',
         () => {
+          // remove animation
           element.classList.remove('animate');
 
-          buttons.forEach((button) => {
-            (button as HTMLButtonElement).disabled = false;
-          });
+          // enable buttons (except for current position)
+          [0, 1, 2]
+            .filter((n) => n !== currentPosition)
+            .forEach((n) => {
+              (
+                document.getElementById(
+                  `color-change-button-${n}`,
+                ) as HTMLButtonElement
+              ).disabled = false;
+            });
         },
         { once: true },
       );
@@ -61,20 +81,23 @@ const ColorChange = () => {
     <section>
       <h2>Change Colors...?</h2>
       <button
+        id="color-change-button-0"
         className="color-change-button"
-        onClick={() => changeGradient('0% 0%')}
+        onClick={() => changeGradient(0)}
       >
         Orange
       </button>
       <button
+        id="color-change-button-1"
         className="color-change-button"
-        onClick={() => changeGradient('33% 33%')}
+        onClick={() => changeGradient(1)}
       >
         Purple
       </button>
       <button
+        id="color-change-button-2"
         className="color-change-button"
-        onClick={() => changeGradient('67% 67%')}
+        onClick={() => changeGradient(2)}
       >
         Green
       </button>
